e05ac7df57c63a12d412f554f7d70c24ad8e3bc2
ErfanRad <raderfann@gmail.com>
2025-11-19 01:14:06 -0800

    Fixed bug

--- Karyabi/settings.py
+++ Karyabi/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'Karyabi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'karyabi_db',
+        'USER': 'karyabi_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
