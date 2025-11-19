469ad8cf0ceaf93c96cc6092b4ff6e4477bc63ae
ErfanRad <raderfann@gmail.com>
2025-11-19 01:27:58 -0800

    commit

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,15 +3,19 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, JobPosition, Resume
 
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('نقش کاربر', {'fields': ('is_employer', 'is_jobseeker')}),
+    )
+    list_display = UserAdmin.list_display + ('is_employer', 'is_jobseeker')
 
+admin.site.register(User, CustomUserAdmin)
 
 @admin.register(JobPosition)
 class JobPositionAdmin(admin.ModelAdmin):
     list_display = ('title', 'employer', 'salary_min', 'created_at')
     search_fields = ('title', 'requirements')
     list_filter = ('created_at',)
-
 
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
